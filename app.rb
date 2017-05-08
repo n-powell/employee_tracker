b45f2bf05b76eda11142121db9da1ff0d9fb3a93
James Higgins and Nick Powell <james@>
2017-05-08 16:22:55 -0700

    add employees to divisions

--- app.rb
+++ app.rb
@@ -1,20 +1,28 @@
 require 'sinatra'
 require 'sinatra/reloader'
-require("sinatra/activerecord")
+require 'sinatra/activerecord'
+require './lib/employee'
 require './lib/division'
 require 'pry'
-
 also_reload('lib/**/*.rb')
 
+
 get('/') do
-    @all_employees = Employee.all
-    @all_divisions = Division.all
+  @all_employees = Employee.all
+  @all_divisions = Division.all
   erb(:index)
 end
 
-
 get('/employee/add') do
   erb(:add_employee_form)
+end
+
+post('/employee/add') do
+  name = params.fetch('employee')
+  Employee.create({:name => name})
+  @all_divisions = Division.all
+  @all_employees = Employee.all
+  erb(:index)
 end
 
 get('/division/add') do
@@ -29,10 +37,30 @@ post('/division/add') do
   erb(:index)
 end
 
-post('/employee/add') do
-  name = params.fetch('employee')
-  Employee.create({:name => name})
-  @all_divisions = Division.all
+get('/division/:id') do
+  @employees = Employee.all
+  @division = Division.find(params.fetch('id').to_i)
+  erb(:division)
+end
+
+patch('/division/:id/employee') do
   @all_employees = Employee.all
+  @all_divisions = Division.all
+  @employee = params['employee'].to_i
+  @division = Division.find(params.fetch('id').to_i)
+  current_employee = Employee.find(@employee)
+  current_employee.update(division_id: @division.id)
   erb(:index)
 end
+# get('/divisions/:id/edit') do
+#   @division = Division.find(params.fetch("id").to_i())
+#   erb(:division_edit)
+# end
+#
+# patch("/divisions/:id") do
+#   division = params.fetch("division")
+#   @division = Division.find(params.fetch("id").to_i())
+#   @division.update({:division => division})
+#   @divisions = Division.all()
+#   erb(:output)
+# end
